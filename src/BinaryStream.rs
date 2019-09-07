071165325e4ef09148b2499566612ec64d4476d1
djdisodo <djdisodo@gmail.com>
2019-09-08 02:05:11 +0900

    waiting for test...

--- src/BinaryStream.rs
+++ src/BinaryStream.rs
@@ -4,7 +4,7 @@ use std::fmt::Error;
 use std::convert::TryInto;
 use std::iter::FromIterator;
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt, LittleEndian};
-use crate::binary::{read_short, read_signed_short, write_short, write_triad, read_triad, read_int, Endian};
+use crate::binary::*;
 
 struct BinaryStream {
 	offset : usize,
@@ -39,7 +39,7 @@ impl BinaryStream {
 	}
 	fn get(&mut self, len : usize) -> Vec<u8> {
 		if self.get_buffer().len() < (self.get_offset() + len) {
-	panic!("BinaryDataException: No bytes left to read");
+			panic!("BinaryDataException: No bytes left to read");
 		}
 		let mut result : Vec<u8> = Vec::from_iter(self.get_buffer().clone().drain(..self.get_offset()));
 		result.resize(len, 0);
@@ -68,22 +68,100 @@ impl BinaryStream {
 	fn put_byte(&mut self, v : u8) {
 		self.buffer.push(v);
 	}
-	fn get_short<T>(&mut self, endian : Endian) -> u16 {
+	fn get_short(&mut self, endian : Endian) -> i16 {
 		return read_short(self.get(2), endian);
 	}
-	fn get_signed_short<T>(&mut self, endian : Endian) -> i16 {
-		return read_signed_short(self.get(2), endian)
+	fn get_unsigned_short(&mut self, endian : Endian) -> u16 {
+		return read_unsigned_short(self.get(2), endian);
 	}
-	fn put_short<T>(&mut self, v : u16, endian : Endian) {
+	fn put_short(&mut self, v : i16, endian : Endian) {
 		self.put(Vec::from(write_short(v, endian)));
 	}
-	fn get_triad<T>(&mut self, endian : Endian) -> u32 {
+	fn put_unsigned_short(&mut self, v : u16, endian : Endian) {
+		self.put(Vec::from(write_unsigned_short(v, endian)));
+	}
+	fn get_triad(&mut self, endian : Endian) -> i32 {
 		return read_triad(self.get(3), endian);
 	}
-	fn put_triad<T>(&mut self, v : u32, endian : Endian) {
+	fn get_unsigned_triad(&mut self, endian : Endian) -> u32 {
+		return read_unsigned_triad(self.get(3), endian);
+	}
+	fn put_triad(&mut self, v : i32, endian : Endian) {
 		self.put(write_triad(v, endian).to_vec());
 	}
-	fn get_int<T>(&mut self, endian : Endian) -> i32 {
+	fn put_unsigned_triad(&mut self, v : u32, endian : Endian) {
+		self.put(write_unsigned_triad(v, endian).to_vec());
+	}
+	fn get_int(&mut self, endian : Endian) -> i32 {
 		return read_int(self.get(4), endian);
+	}
+	fn get_unsigned_int(&mut self, endian : Endian) -> u32 {
+		return read_unsigned_int(self.get(4), endian);
+	}
+	fn put_int(&mut self, v : i32, endian : Endian) {
+		self.put(write_int(v, endian).to_vec());
+	}
+	fn put_unsigned_int(&mut self, v : u32, endian : Endian) {
+		self.put(write_unsigned_int(v, endian).to_vec());
+	}
+	fn get_float(&mut self, endian : Endian) -> f32 {
+		return read_float(self.get(4), endian);
+	}
+	fn put_float(&mut self, v : f32, endian : Endian) {
+		self.put(write_float(v, endian).to_vec());
+	}
+	fn get_double(&mut self, endian : Endian) -> f64 {
+		return read_double(self.get(8), endian);
+	}
+	fn get_long(&mut self, endian : Endian) -> i64 {
+		return read_long(self.get(8), endian);
+	}
+	fn get_unsigned_long(&mut self, endian : Endian) -> u64 {
+		return read_unsigned_long(self.get(8), endian);
+	}
+	fn put_long(&mut self, v : i64, endian : Endian) {
+		self.put(write_long(v, endian).to_vec());
+	}
+	fn put_unsigned_long(&mut self, v : u64, endian : Endian) {
+		self.put(write_unsigned_long(v, endian).to_vec());
+	}
+	fn get_unsigned_var_int(&mut self, endian : Endian) -> u32 {
+		let mut read_bytes  : u8 = 0;
+		let result : u32 = read_unsigned_var_int(Vec::from_iter(self.get_buffer().clone().drain(..self.get_offset())), &mut read_bytes);
+		self.offset.add(read_bytes as usize);
+		return result;
+	}
+	fn put_unsigned_var_int(&mut self, v : u32, endian : Endian) {
+		self.put(write_unsigned_var_int(v).to_vec());
+	}
+	fn get_var_int(&mut self, endian : Endian) -> i32 {
+		let mut read_bytes  : u8 = 0;
+		let result : i32 = read_var_int(Vec::from_iter(self.get_buffer().clone().drain(..self.get_offset())), &mut read_bytes);
+		self.offset.add(read_bytes as usize);
+		return result;
+	}
+	fn put_var_int(&mut self, v : i32, endian : Endian) {
+		self.put(write_var_int(v).to_vec());
+	}
+	fn get_unsigned_var_long(&mut self, endian : Endian) -> u64 {
+		let mut read_bytes  : u8 = 0;
+		let result : u64 = read_unsigned_var_long(Vec::from_iter(self.get_buffer().clone().drain(..self.get_offset())), &mut read_bytes);
+		self.offset.add(read_bytes as usize);
+		return result;
+	}
+	fn put_unsigned_var_long(&mut self, v : u64, endian : Endian) {
+		self.put(write_unsigned_var_long(v).to_vec());
+	}
+	fn get_var_long(&mut self, endian : Endian) -> i64 {
+		let mut read_bytes  : u8 = 0;
+		let result : i64 = read_var_long(Vec::from_iter(self.get_buffer().clone().drain(..self.get_offset())), &mut read_bytes);
+		self.offset.add(read_bytes as usize);
+		return result;
+	}
+	fn put_var_long(&mut self, v : i64, endian : Endian) {
+		self.put(write_var_long(v).to_vec());
+	}
+	fn feof(&self) -> bool{
+		return self.get_buffer().len() < self.get_offset();
 	}
 }
--- src/binary.rs
+++ src/binary.rs
@@ -1,36 +1,62 @@
 extern crate byteorder;
-use byteorder::BigEndian;
-use std::io::Cursor;
-use self::byteorder::{ReadBytesExt, WriteBytesExt, LittleEndian};
-use std::any::{Any, TypeId};
-use crate::binary::Endian::{Big, Little};
 
-pub enum Endian{
+use std::io::Cursor;
+use std::ops::{BitOrAssign, Add};
+use self::byteorder::{ReadBytesExt, WriteBytesExt, BigEndian, LittleEndian};
+
+#[derive(Clone, Copy)]
+pub enum Endian {
 	Big = 0,
-	Little = 1
+	Little = 1,
 }
-pub fn read_short(bytes : Vec<u8>, endian : Endian) -> u16{
-	match endian {
-		Big => return Cursor::read_u16::<BigEndian>(&mut Cursor::new(bytes)).unwrap(),
-		Little => return Cursor::read_u16::<LittleEndian>(&mut Cursor::new(bytes)).unwrap()
-	}
-}
-pub fn read_signed_short(bytes : Vec<u8>, endian : Endian) -> i16{
+
+pub fn read_short(bytes: Vec<u8>, endian: Endian) -> i16 {
 	match endian {
 		Big => return Cursor::read_i16::<BigEndian>(&mut Cursor::new(bytes)).unwrap(),
 		Little => return Cursor::read_i16::<LittleEndian>(&mut Cursor::new(bytes)).unwrap()
 	}
 }
-pub fn write_short(v : u16, endian : Endian) -> Vec<u8>{
-	let mut bytes : Vec<u8> = Vec::new();
+
+pub fn read_unsigned_short(bytes: Vec<u8>, endian: Endian) -> u16 {
+	match endian {
+		Big => return Cursor::read_u16::<BigEndian>(&mut Cursor::new(bytes)).unwrap(),
+		Little => return Cursor::read_u16::<LittleEndian>(&mut Cursor::new(bytes)).unwrap()
+	}
+}
+
+pub fn write_short(v: i16, endian: Endian) -> Vec<u8> {
+	let mut bytes: Vec<u8> = Vec::new();
+	match endian {
+		Big => bytes.write_i16::<BigEndian>(v).unwrap(),
+		Little => bytes.write_i16::<LittleEndian>(v).unwrap()
+	}
+	return bytes;
+}
+
+pub fn write_unsigned_short(v: u16, endian: Endian) -> Vec<u8> {
+	let mut bytes: Vec<u8> = Vec::new();
 	match endian {
 		Big => bytes.write_u16::<BigEndian>(v).unwrap(),
 		Little => bytes.write_u16::<LittleEndian>(v).unwrap()
 	}
 	return bytes;
 }
-pub fn read_triad(bytes : Vec<u8>, endian : Endian) -> u32{
-	let mut bytes : Vec<u8> = Vec::from(bytes);
+
+pub fn read_triad(bytes: Vec<u8>, endian: Endian) -> i32 {
+	let mut bytes: Vec<u8> = Vec::from(bytes);
+	match endian {
+		Little => bytes.reverse(),
+		_ => {}
+	}
+	bytes.push(0);
+	match endian {
+		Big => return Cursor::read_i32::<BigEndian>(&mut Cursor::new(bytes)).unwrap(),
+		Little => return Cursor::read_i32::<LittleEndian>(&mut Cursor::new(bytes)).unwrap()
+	}
+}
+
+pub fn read_unsigned_triad(bytes: Vec<u8>, endian: Endian) -> u32 {
+	let mut bytes: Vec<u8> = Vec::from(bytes);
 	match endian {
 		Little => bytes.reverse(),
 		_ => {}
@@ -41,13 +67,31 @@ pub fn read_triad(bytes : Vec<u8>, endian : Endian) -> u32{
 		Little => return Cursor::read_u32::<LittleEndian>(&mut Cursor::new(bytes)).unwrap()
 	}
 }
-pub fn write_triad(v : u32, endian : Endian) -> Vec<u8>{
-	let mut bytes : Vec<u8> = Vec::new();
+
+pub fn write_triad(v: i32, endian: Endian) -> Vec<u8> {
+	let mut bytes: Vec<u8> = Vec::new();
+	match endian {
+		Big => {
+			bytes.write_i32::<BigEndian>(v).unwrap();
+			bytes.resize(3, 0);
+		}
+		Little => {
+			bytes.write_i32::<LittleEndian>(v).unwrap();
+			bytes.reverse();
+			bytes.resize(3, 0);
+			bytes.reverse();
+		}
+	}
+	return bytes;
+}
+
+pub fn write_unsigned_triad(v: u32, endian: Endian) -> Vec<u8> {
+	let mut bytes: Vec<u8> = Vec::new();
 	match endian {
 		Big => {
 			bytes.write_u32::<BigEndian>(v).unwrap();
 			bytes.resize(3, 0);
-		},
+		}
 		Little => {
 			bytes.write_u32::<LittleEndian>(v).unwrap();
 			bytes.reverse();
@@ -57,17 +101,177 @@ pub fn write_triad(v : u32, endian : Endian) -> Vec<u8>{
 	}
 	return bytes;
 }
-pub fn read_int(bytes : Vec<u8>, endian : Endian) -> i32{
+
+pub fn read_int(bytes: Vec<u8>, endian: Endian) -> i32 {
 	match endian {
 		Big => return Cursor::read_i32::<BigEndian>(&mut Cursor::new(bytes)).unwrap(),
 		Little => return Cursor::read_i32::<LittleEndian>(&mut Cursor::new(bytes)).unwrap()
 	}
 }
-pub fn write_int(v : i32, endian : Endian) -> Vec<u8>{
-	let mut bytes : Vec<u8> = Vec::new();
+
+pub fn read_unsigned_int(bytes: Vec<u8>, endian: Endian) -> u32 {
+	match endian {
+		Big => return Cursor::read_u32::<BigEndian>(&mut Cursor::new(bytes)).unwrap(),
+		Little => return Cursor::read_u32::<LittleEndian>(&mut Cursor::new(bytes)).unwrap()
+	}
+}
+
+pub fn write_int(v: i32, endian: Endian) -> Vec<u8> {
+	let mut bytes: Vec<u8> = Vec::new();
 	match endian {
 		Big => bytes.write_i32::<BigEndian>(v).unwrap(),
 		Little => bytes.write_i32::<LittleEndian>(v).unwrap()
 	}
 	return bytes;
+}
+
+pub fn write_unsigned_int(v: u32, endian: Endian) -> Vec<u8> {
+	let mut bytes: Vec<u8> = Vec::new();
+	match endian {
+		Big => bytes.write_u32::<BigEndian>(v).unwrap(),
+		Little => bytes.write_u32::<LittleEndian>(v).unwrap()
+	}
+	return bytes;
+}
+
+pub fn read_float(bytes: Vec<u8>, endian: Endian) -> f32 {
+	match endian {
+		Big => return Cursor::read_f32::<BigEndian>(&mut Cursor::new(bytes)).unwrap(),
+		Little => return Cursor::read_f32::<LittleEndian>(&mut Cursor::new(bytes)).unwrap()
+	}
+}
+
+pub fn write_float(v: f32, endian: Endian) -> Vec<u8> {
+	let mut bytes: Vec<u8> = Vec::new();
+	match endian {
+		Big => bytes.write_f32::<BigEndian>(v).unwrap(),
+		Little => bytes.write_f32::<LittleEndian>(v).unwrap()
+	}
+	return bytes;
+}
+
+pub fn read_double(bytes: Vec<u8>, endian: Endian) -> f64 {
+	match endian {
+		Big => return Cursor::read_f64::<BigEndian>(&mut Cursor::new(bytes)).unwrap(),
+		Little => return Cursor::read_f64::<LittleEndian>(&mut Cursor::new(bytes)).unwrap()
+	}
+}
+
+pub fn write_double(v: f64, endian: Endian) -> Vec<u8> {
+	let mut bytes: Vec<u8> = Vec::new();
+	match endian {
+		Big => bytes.write_f64::<BigEndian>(v).unwrap(),
+		Little => bytes.write_f64::<LittleEndian>(v).unwrap()
+	}
+	return bytes;
+}
+
+pub fn read_long(bytes: Vec<u8>, endian: Endian) -> i64 {
+	match endian {
+		Big => return Cursor::read_i64::<BigEndian>(&mut Cursor::new(bytes)).unwrap(),
+		Little => return Cursor::read_i64::<LittleEndian>(&mut Cursor::new(bytes)).unwrap()
+	}
+}
+
+pub fn read_unsigned_long(bytes: Vec<u8>, endian: Endian) -> u64 {
+	match endian {
+		Big => return Cursor::read_u64::<BigEndian>(&mut Cursor::new(bytes)).unwrap(),
+		Little => return Cursor::read_u64::<LittleEndian>(&mut Cursor::new(bytes)).unwrap()
+	}
+}
+
+pub fn write_long(v: i64, endian: Endian) -> Vec<u8> {
+	let mut bytes: Vec<u8> = Vec::new();
+	match endian {
+		Big => bytes.write_i64::<BigEndian>(v).unwrap(),
+		Little => bytes.write_i64::<LittleEndian>(v).unwrap()
+	}
+	return bytes;
+}
+
+pub fn write_unsigned_long(v: u64, endian: Endian) -> Vec<u8> {
+	let mut bytes: Vec<u8> = Vec::new();
+	match endian {
+		Big => bytes.write_u64::<BigEndian>(v).unwrap(),
+		Little => bytes.write_u64::<LittleEndian>(v).unwrap()
+	}
+	return bytes;
+}
+
+pub fn read_var_int(bytes: Vec<u8>, read_bytes : &mut u8 /* stores counts of bytes readed (starts from zero) */) -> i32 {
+	let raw: u32 = read_unsigned_var_int(bytes, read_bytes);
+	let temp: u32 = (((raw << 31) >> 31) ^ raw) >> 1;
+	return (temp ^ (raw & (1 << 31))) as i32;
+}
+
+pub fn read_unsigned_var_int(bytes: Vec<u8>, read_bytes : &mut u8 /* stores counts of bytes readed (starts from zero) */) -> u32 {
+	let mut v: u32 = 0;
+	for i in 0..5 {
+		if bytes.len() < i {
+			panic!("BinaryDataException: No bytes left to read");
+		}
+		v.bitor_assign(((bytes.get(i).unwrap().clone() as u32) & 0x7f) << (i * 7) as u32);
+		if bytes.get(i).unwrap() & 0x80 == 0 {
+			read_bytes.add(i as u8);
+			return v;
+		}
+	}
+	panic!("BinaryDataException : VarInt did not terminate after 5 bytes!");
+}
+
+pub fn write_var_int(v: i32) -> Vec<u8> {
+	return write_unsigned_var_int((v << 1 ^ (v >> 31)) as u32);
+}
+
+pub fn write_unsigned_var_int(mut v: u32) -> Vec<u8> {
+	let mut bytes: Vec<u8> = Vec::new();
+	for i in 0..5 {
+		if (v >> 7) != 0 {
+			bytes.push((v | 0x80) as u8);
+		} else {
+			bytes.push((v | 0x7f) as u8);
+			return bytes;
+		}
+		v >>= 7;
+	}
+	panic!("InvalidArgumentException : Value too large to ve encoded as a VarInt")
+}
+
+pub fn read_var_long(bytes: Vec<u8>, read_bytes : &mut u8 /* stores counts of bytes readed (starts from zero) */) -> i64 {
+	let raw: u64 = read_unsigned_var_long(bytes, read_bytes);
+	let temp: u64 = (((raw << 63) >> 63) ^ raw) >> 1;
+	return (temp ^ (raw & (1 << 63))) as i64;
+}
+
+pub fn read_unsigned_var_long(bytes: Vec<u8>, read_bytes : &mut u8 /* stores counts of bytes readed (starts from zero) */) -> u64 {
+	let mut v: u64 = 0;
+	for i in 0..10 {
+		if bytes.len() < i {
+			panic!("BinaryDataException: No bytes left to read");
+		}
+		v.bitor_assign(((bytes.get(i).unwrap().clone() as u64) & 0x7f) << (i * 7) as u64);
+		if bytes.get(i).unwrap() & 0x80 == 0 {
+			read_bytes.add(i as u8);
+			return v;
+		}
+	}
+	panic!("BinaryDataException : VarInt did not terminate after 10 bytes!");
+}
+
+pub fn write_var_long(v: i64) -> Vec<u8> {
+	return write_unsigned_var_long((v << 1 ^ (v >> 63)) as u64);
+}
+
+pub fn write_unsigned_var_long(mut v: u64) -> Vec<u8> {
+	let mut bytes: Vec<u8> = Vec::new();
+	for i in 0..10 {
+		if (v >> 7) != 0 {
+			bytes.push((v | 0x80) as u8);
+		} else {
+			bytes.push((v | 0x7f) as u8);
+			return bytes;
+		}
+		v >>= 7;
+	}
+	panic!("InvalidArgumentException : Value too large to ve encoded as a VarInt")
 }
