cb5540f6cfc724ed4efe6e26ab674b71f9bb878d
djdisodo <djdisodo@gmail.com>
2019-09-21 10:55:37 +0900

    afja

--- src/binary_stream.rs
+++ src/binary_stream.rs
@@ -1,6 +1,7 @@
 use std::io::Write;
 use std::iter::FromIterator;
 use crate::binary::*;
+use std::ops::Range;
 
 pub struct BinaryStream {
 	buffer: Vec<u8>,
@@ -38,7 +39,7 @@ impl BinaryStream {
 		if self.get_buffer().len() < (self.get_offset() + len) {
 			panic!("BinaryDataException: No bytes left to read");
 		}
-		let mut result: Vec<u8> = Vec::from_iter(self.get_buffer().clone().drain(..self.get_offset()));
+		let mut result: Vec<u8> = Vec::from_iter(self.get_buffer().drain(self.get_offset()..(self.get_offset() + len)));
 		result.truncate(len);
 		self.offset += len;
 		return result;
@@ -47,8 +48,10 @@ impl BinaryStream {
 		if self.get_buffer().len() < self.get_offset() {
 			panic!("BinaryDataException: No bytes left to read");
 		}
+		
+		let v : Vec<u8> = Vec::from_iter(self.get_buffer().drain(self.get_offset()..));
 		self.set_offset(self.get_buffer().len());
-		return Vec::from_iter(self.get_buffer().clone().drain(..self.get_offset()));
+		return v;
 	}
 	pub fn put(&mut self, bytes: Vec<u8>) {
 		self.buffer.extend(bytes);
